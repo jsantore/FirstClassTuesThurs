fa21015348458b4048028a193f9496e559f3ca2b
shinjitsu <shinjitsu@auxilioabalto.me>
2026-09-10 12:21:22 -0400

    added strings examples

---
+++ StringsExample.py
@@ -0,0 +1,9 @@
+example1 = 'she said "I will do it" '
+example2 = " I'm going to use a contraction"
+example3 = """Use this 
+if you want to
+go multiline"""
+
+favorite_class = input("What is your favorite class? ")
+loud_version = favorite_class.upper()
+print(" Your favorite class is ", loud_version)
---
+++ strings2.py
@@ -0,0 +1,3 @@
+class_name = input("what class are you taking next semester:")
+num_credits = input("How many credits is it:")
+print(f"You are taking class_name next semester for {num_credits} credits")
